2d2d7cf0ef32f198ac7a0ca181766c1e57461a1b
antonharkevich <61267091+antonharkevich@users.noreply.github.com>
2020-04-27 01:36:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace ConsoleApp4
+{
+    class Program
+    {
+        [DllImport("C:\\dllForC#\\sumofarray.dll", CallingConvention = CallingConvention.Cdecl)]
+        static extern int Sum(int[] x, int n);
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+            int c = Sum(a,a.Length);
+            Console.WriteLine("{0}", c);
+        }
+    }
+}
